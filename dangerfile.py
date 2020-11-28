0b23dfba2d84ecf36b2e5dc554ac66d8358cc5a9
Delena Malan <delena.malan@gmail.com>
2020-11-28 17:45:24 +0200

    Check if this works

--- dangerfile.py
+++ dangerfile.py
@@ -1,7 +1,10 @@
 """
 Used for automated data checks.
 """
-from danger_python import danger, markdown
+touched_files = danger.git.modified_files + danger.git.created_files
+has_source_changes = any(map(lambda f: f.startswith("danger_python"), touched_files))
+has_changelog_entry = "CHANGELOG.md" in touched_files
+is_trivial = "#trivial" in danger.github.pr.title
 
-title = danger.github.pr.title
-markdown(title)
+if has_source_changes and not has_changelog_entry and not is_trivial:
+    warn("Please, add a CHANGELOG.md entry for non-trivial changes")
